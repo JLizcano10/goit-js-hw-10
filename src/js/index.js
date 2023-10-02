ef6bc5b973e2819140c7a368b3cc6d9ce8f006c0
JL10 <126992358+JLizcano10@users.noreply.github.com>
2023-10-02 17:55:34 -0500

    functions create and querySelectors defined

---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+const select = document.querySelector('select.breed-select');
+const loader = document.querySelector('p.loader');
+const errorMessage = document.querySelector('p.error');
+const catInfo = document.querySelector('div.cat-info');
+console.log(catInfo);
