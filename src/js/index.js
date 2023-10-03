4cae6ed55d81e4748841e37b8180363e37e4f370
JL10 <126992358+JLizcano10@users.noreply.github.com>
2023-10-03 18:02:28 -0500

    install SlimSelect and Notiflix

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,12 @@
+import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
+import Notiflix from 'notiflix';
 const select = document.querySelector('select.breed-select');
 const loader = document.querySelector('p.loader');
 const errorMessage = document.querySelector('p.error');
 const catInfo = document.querySelector('div.cat-info');
 console.log(catInfo);
+
+new SlimSelect({
+  select: select,
+});
