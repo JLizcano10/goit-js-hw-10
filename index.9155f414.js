89417ad4e8172d3a465be71d2853d916ae08c1ee
JLizcano10 <126992358+JLizcano10@users.noreply.github.com>
2023-10-03 23:02:58 +0000

    Deploying to gh-pages from @ JLizcano10/goit-js-hw-10@4cae6ed55d81e4748841e37b8180363e37e4f370 🚀

--- index.9155f414.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("select.breed-select"),document.querySelector("p.loader"),document.querySelector("p.error");const e=document.querySelector("div.cat-info");console.log(e);
-//# sourceMappingURL=index.9155f414.js.map
--- index.f311f29c.js
+++
@@ -1,2 +0,0 @@
-!function(){document.querySelector("select.breed-select"),document.querySelector("p.loader"),document.querySelector("p.error");var e=document.querySelector("div.cat-info");console.log(e)}();
-//# sourceMappingURL=index.f311f29c.js.map
