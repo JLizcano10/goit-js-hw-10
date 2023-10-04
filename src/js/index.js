70cefa14b28071b02e02486eea250608824a11c0
JL10 <126992358+JLizcano10@users.noreply.github.com>
2023-10-04 00:42:52 -0500

    select element styles

--- src/js/index.js
+++ src/js/index.js
@@ -7,12 +7,17 @@ const loader = document.querySelector('p.loader');
 const errorMessage = document.querySelector('p.error');
 const catInfo = document.querySelector('div.cat-info');
 
+select.style.display = 'none';
+
 fetchBreeds().then(data => {
   console.log(data);
   const optionSelect = data
     .map(object => `<option value="${object.id}">${object.name}</option>`)
     .join('');
   select.insertAdjacentHTML('beforeend', optionSelect);
+
+  select.style.display = 'flex';
+
   // Recordar que debo cargarlo aqui porque si lo pongo afuera no coge los valores de la promesa (asicronia)
   new SlimSelect({
     select: select,
